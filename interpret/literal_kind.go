e89dffa072076dcd19b0e9f9298f3b4635ff4abe
x0y14 <ya01ma08@gmail.com>
2022-08-31 10:56:02 +0900

    string関数を追加

--- interpret/literal_kind.go
+++ interpret/literal_kind.go
@@ -15,3 +15,20 @@ const (
 	False
 	Null
 )
+
+var literalKinds = [...]string{
+	Ident:  "Ident",
+	Float:  "Float",
+	Int:    "Int",
+	String: "String",
+	Raw:    "Raw",
+	Array:  "Array",
+	Dict:   "Dict",
+	True:   "True",
+	False:  "False",
+	Null:   "Null",
+}
+
+func (l LiteralKind) String() string {
+	return literalKinds[l]
+}
