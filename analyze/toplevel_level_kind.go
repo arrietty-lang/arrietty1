1829b9bc75c0da7bd6780fbb6405fec4d3a06845
x0y14 <ya01ma08@gmail.com>
2022-09-09 14:17:27 +0900

    いらないtp-kindを削除

--- analyze/toplevel_level_kind.go
+++ analyze/toplevel_level_kind.go
@@ -4,105 +4,13 @@ type ToplevelKind int
 
 const (
 	_ ToplevelKind = iota
-	TPReturn
-	TPIf
-	TPIfElse
-	TPWhile
-	TPFor
-
-	TPNot
-	TPPlus
-	TPMinus
-
-	TPAnd
-	TPOr
-
-	TPEq
-	TPNe
-	TPLt
-	TPLe
-	TPGt
-	TPGe
-
-	TPAdd
-	TPSub
-	TPMul
-	TPDiv
-	TPMod
-
 	TPFuncDef
-	TPParams
-	TPParam
-
-	TPVarDecl
-	TPAssign
-
-	TPIdent
-	TPCall
-	TPArgs
-
-	TPFloat
-	TPInt
-	TPString // raw string...?
-	TPList
-	TPDict
-	TPKv
-	TPBool
-	TPTrue
-	TPFalse
-	TPVoid
-	TPNull
-
 	TPComment
-
-	TPAccess      // []
-	TPParenthesis // ()
 )
 
 var toplevelKinds = [...]string{
-	TPReturn:      "Return",
-	TPIf:          "If",
-	TPIfElse:      "IfElse",
-	TPWhile:       "While",
-	TPFor:         "For",
-	TPNot:         "Not",
-	TPPlus:        "Plus",
-	TPMinus:       "Minus",
-	TPAnd:         "And",
-	TPOr:          "Or",
-	TPEq:          "Eq",
-	TPNe:          "Ne",
-	TPLt:          "Lt",
-	TPLe:          "Le",
-	TPGt:          "Gt",
-	TPGe:          "Ge",
-	TPAdd:         "Add",
-	TPSub:         "Sub",
-	TPMul:         "Mul",
-	TPDiv:         "Div",
-	TPMod:         "Mod",
-	TPFuncDef:     "FuncDef",
-	TPParams:      "Params",
-	TPParam:       "Param",
-	TPVarDecl:     "VarDecl",
-	TPAssign:      "AssignTo",
-	TPIdent:       "Ident",
-	TPCall:        "Call",
-	TPArgs:        "Args",
-	TPFloat:       "Float",
-	TPInt:         "Int",
-	TPString:      "String",
-	TPList:        "List",
-	TPDict:        "Dict",
-	TPKv:          "Kv",
-	TPBool:        "Bool",
-	TPTrue:        "True",
-	TPFalse:       "False",
-	TPVoid:        "Void",
-	TPNull:        "Null",
-	TPComment:     "Comment",
-	TPAccess:      "Access",
-	TPParenthesis: "Parenthesis",
+	TPFuncDef: "FuncDef",
+	TPComment: "Comment",
 }
 
 func (t ToplevelKind) String() string {
