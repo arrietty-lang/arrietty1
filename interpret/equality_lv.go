3c48ee8902422637cd8ff368b77ed87ba07ce418
x0y14 <ya01ma08@gmail.com>
2022-09-10 17:36:21 +0900

    =, !=を実装

--- interpret/equality_lv.go
+++ interpret/equality_lv.go
@@ -10,8 +10,25 @@ func evalEquality(mem *Memory, equalityLv *analyze.EqualityLevel) (*Object, erro
 	case analyze.EQRelationalLevel:
 		return evalRelation(mem, equalityLv.RelationalLevel)
 	case analyze.EQEqual:
+		lhs, err := evalEquality(mem, equalityLv.LHS)
+		if err != nil {
+			return nil, err
+		}
+		rhs, err := evalEquality(mem, equalityLv.RHS)
+		if err != nil {
+			return nil, err
+		}
+		return eq(lhs, rhs)
 	case analyze.EQNotEqual:
-
+		lhs, err := evalEquality(mem, equalityLv.LHS)
+		if err != nil {
+			return nil, err
+		}
+		rhs, err := evalEquality(mem, equalityLv.RHS)
+		if err != nil {
+			return nil, err
+		}
+		return ne(lhs, rhs)
 	}
 	return nil, fmt.Errorf("unimplemented: %s", equalityLv.Kind.String())
 }
