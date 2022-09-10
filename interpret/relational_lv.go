a1a5f7723cb94e3aed73e25218637c6261ced9eb
x0y14 <ya01ma08@gmail.com>
2022-09-10 17:36:50 +0900

    <, <=, >, >=を実装

--- interpret/relational_lv.go
+++ interpret/relational_lv.go
@@ -10,10 +10,45 @@ func evalRelation(mem *Memory, relationalLv *analyze.RelationalLevel) (*Object, 
 	case analyze.REAddLevel:
 		return evalAdd(mem, relationalLv.AddLevel)
 	case analyze.RELt:
+		lhs, err := evalRelation(mem, relationalLv.LHS)
+		if err != nil {
+			return nil, err
+		}
+		rhs, err := evalRelation(mem, relationalLv.RHS)
+		if err != nil {
+			return nil, err
+		}
+		return lt(lhs, rhs)
 	case analyze.RELe:
+		lhs, err := evalRelation(mem, relationalLv.LHS)
+		if err != nil {
+			return nil, err
+		}
+		rhs, err := evalRelation(mem, relationalLv.RHS)
+		if err != nil {
+			return nil, err
+		}
+		return le(lhs, rhs)
 	case analyze.REGt:
+		lhs, err := evalRelation(mem, relationalLv.LHS)
+		if err != nil {
+			return nil, err
+		}
+		rhs, err := evalRelation(mem, relationalLv.RHS)
+		if err != nil {
+			return nil, err
+		}
+		return gt(lhs, rhs)
 	case analyze.REGe:
-
+		lhs, err := evalRelation(mem, relationalLv.LHS)
+		if err != nil {
+			return nil, err
+		}
+		rhs, err := evalRelation(mem, relationalLv.RHS)
+		if err != nil {
+			return nil, err
+		}
+		return ge(lhs, rhs)
 	}
 	return nil, fmt.Errorf("unimplemented: %s", relationalLv.Kind.String())
 }
