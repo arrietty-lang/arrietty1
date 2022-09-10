dacb47091d698052e56fc611d86e9503c9833b4f
x0y14 <ya01ma08@gmail.com>
2022-09-10 17:35:55 +0900

    +, -を実装

--- interpret/add_lv.go
+++ interpret/add_lv.go
@@ -10,8 +10,55 @@ func evalAdd(mem *Memory, addLv *analyze.AddLevel) (*Object, error) {
 	case analyze.ADMulLevel:
 		return evalMul(mem, addLv.MulLevel)
 	case analyze.ADAdd:
+		lhs, err := evalAdd(mem, addLv.LHS)
+		if err != nil {
+			return nil, err
+		}
+		rhs, err := evalAdd(mem, addLv.RHS)
+		if err != nil {
+			return nil, err
+		}
+		// どちらもInt
+		if lhs.Kind == OInt && rhs.Kind == OInt {
+			return NewIntObject(lhs.I + rhs.I), nil
+		}
+		// どちらもFloat
+		if lhs.Kind == OFloat && rhs.Kind == OFloat {
+			return NewFloatObject(lhs.F + rhs.F), nil
+		}
+		// int + float
+		if lhs.Kind == OInt && rhs.Kind == OFloat {
+			return NewFloatObject(float64(lhs.I) + rhs.F), nil
+		}
+		// float + int
+		if lhs.Kind == OFloat && rhs.Kind == OInt {
+			return NewFloatObject(lhs.F + float64(rhs.I)), nil
+		}
 	case analyze.ADSub:
-
+		lhs, err := evalAdd(mem, addLv.LHS)
+		if err != nil {
+			return nil, err
+		}
+		rhs, err := evalAdd(mem, addLv.RHS)
+		if err != nil {
+			return nil, err
+		}
+		// どちらもInt
+		if lhs.Kind == OInt && rhs.Kind == OInt {
+			return NewIntObject(lhs.I - rhs.I), nil
+		}
+		// どちらもFloat
+		if lhs.Kind == OFloat && rhs.Kind == OFloat {
+			return NewFloatObject(lhs.F - rhs.F), nil
+		}
+		// int - float
+		if lhs.Kind == OInt && rhs.Kind == OFloat {
+			return NewFloatObject(float64(lhs.I) - rhs.F), nil
+		}
+		// float - int
+		if lhs.Kind == OFloat && rhs.Kind == OInt {
+			return NewFloatObject(lhs.F - float64(rhs.I)), nil
+		}
 	}
 	return nil, fmt.Errorf("unimplemented: %s", addLv.Kind.String())
 }
