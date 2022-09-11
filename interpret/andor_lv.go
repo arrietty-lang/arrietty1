cdcc541414e87ae3e46d57e97507d20cce32bc04
x0y14 <ya01ma08@gmail.com>
2022-09-11 10:52:32 +0900

    &&, ||を実装

--- interpret/andor_lv.go
+++ interpret/andor_lv.go
@@ -10,8 +10,43 @@ func evalAndOr(mem *Memory, andorLv *analyze.AndOrLevel) (*Object, error) {
 	case analyze.ANEqualityLevel:
 		return evalEquality(mem, andorLv.EqualityLevel)
 	case analyze.ANAnd:
+		lhs, err := evalAndOr(mem, andorLv.LHS)
+		if err != nil {
+			return nil, err
+		}
+		rhs, err := evalAndOr(mem, andorLv.RHS)
+		if err != nil {
+			return nil, err
+		}
+		switch {
+		case lhs.Kind == OTrue && rhs.Kind == OTrue:
+			return NewTrueObject(), nil
+		case lhs.Kind == OTrue && rhs.Kind == OFalse:
+			return NewFalseObject(), nil
+		case lhs.Kind == OFalse && rhs.Kind == OTrue:
+			return NewFalseObject(), nil
+		case lhs.Kind == OFalse && rhs.Kind == OFalse:
+			return NewFalseObject(), nil
+		}
 	case analyze.ANOr:
-
+		lhs, err := evalAndOr(mem, andorLv.LHS)
+		if err != nil {
+			return nil, err
+		}
+		rhs, err := evalAndOr(mem, andorLv.RHS)
+		if err != nil {
+			return nil, err
+		}
+		switch {
+		case lhs.Kind == OTrue && rhs.Kind == OTrue:
+			return NewTrueObject(), nil
+		case lhs.Kind == OTrue && rhs.Kind == OFalse:
+			return NewTrueObject(), nil
+		case lhs.Kind == OFalse && rhs.Kind == OTrue:
+			return NewTrueObject(), nil
+		case lhs.Kind == OFalse && rhs.Kind == OFalse:
+			return NewFalseObject(), nil
+		}
 	}
 	return nil, fmt.Errorf("unimplemented: %s", andorLv.Kind.String())
 }
